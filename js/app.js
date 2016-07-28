3a228c29000e47958cdef649787067d266c32563
Carlos Melcon <cmelcon@fvi.edu>
2016-07-27 22:25:45 -0400

    making progress

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 const $ = require('jquery');
+/*$(".img").css("height", window.innerHeight + "px");*/
+$(document).on("click", getResults());
 $(document).on("keypress", function(a){
   console.log(a);
   if (a.keyCode === 13){
-    $.getJSON("http://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=search&exlimit=10&callback=?&exintro=1&gsrsearch=" + $("#search").val())
-    .then(function(resp, txt, xhr){
-      console.log(resp);
-      for (var i in resp.query.pages){
-      $("#result").append(resp.query.pages[i].extract);
-     };
-    });
+    getResults();
   };
 });
+function getResults(){
+  $.getJSON("http://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&titles=Main+Page&generator=search&exlimit=10&exintro=1callback=?&inprop=url&gsrsearch=" + $("#search").val())
+  .then(function(resp, txt, xhr){
+    console.log(resp);
+    for (var i in resp.query.pages){
+    $("#result").append(resp.query.pages[i].extract);
+   };
+  });
+};
