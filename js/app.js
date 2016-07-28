528e83a537a6b1248ee48b89a1d5eb2cf51d561f
cmelcon <cmelcon@fvi.edu>
2016-07-28 02:58:44 -0400

    123

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 const $ = require('jquery');
+var x = ""
 /*$(".img").css("height", window.innerHeight + "px");*/
-$(document).on("click", getResults());
+/*$(document).on("click", getResults());*/
 $(document).on("keypress", function(a){
-  console.log(a);
-  if (a.keyCode === 13){
-    getResults();
-  };
+	console.log(a);
+	if (a.keyCode === 13){
+		getResults();
+	};
 });
 function getResults(){
-  $.getJSON("http://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&titles=Main+Page&generator=search&exlimit=10&exintro=1callback=?&inprop=url&gsrsearch=" + $("#search").val())
-  .then(function(resp, txt, xhr){
-    console.log(resp);
-    for (var i in resp.query.pages){
-    $("#result").append(resp.query.pages[i].extract);
-   };
-  });
+	$.getJSON("http://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&list=&titles=Main+Page&generator=search&exlimit=10&exintro=1&callback=?&inprop=url&gsrsearch=" + $("#search").val())
+	.then(function(resp, txt, xhr){
+		console.log(resp);
+		for (var i in resp.query.pages){
+			$("#result").append("<a href="+resp.query.pages[i].fullurl+">"+resp.query.pages[i].title+"</a>");
+		};
+	});
 };
